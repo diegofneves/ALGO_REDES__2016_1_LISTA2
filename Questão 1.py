825c0152b16ec98295277c23750906268adae246
Diego de França <ufpb.diego@gmail.com>
2016-09-01 09:25:38 -0300

    Add files via upload

---
+++ Questão 1.py
@@ -0,0 +1,21 @@
+regular = 0
+bom = 0
+otimo = 0
+
+for i in range(10):
+    nota = int(input('O que você achou do filme? 3 - ótimo, 2 - bom e 1 - regular: '))
+    if nota == 1:
+        regular = regular + 1
+        print('Regular')
+    elif nota == 2:
+        bom = bom + 1
+        print('Bom')
+    elif nota == 3:
+        otimo = otimo + 1
+        print('Bom')
+    else:
+        print('Opção inválida')
+
+print ('1) %s pessoa(as) respondeu(ram) ótimo.' %otimo)
+print ('2) %s pessoa(as) respondeu(ram) bom.' %bom)
+print ('3) %s pessoa(as) respondeu(ram) regular.' %regular)
